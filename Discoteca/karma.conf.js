03d3e22575777a904b5719219d311b92a96d2d4a
Camilo Rozo <camilo.rozo@ceiba.com.co>
2022-03-24 11:17:56 -0500

    fix karma

--- Discoteca/karma.conf.js
+++ Discoteca/karma.conf.js
@@ -8,47 +8,37 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-junit-reporter'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    remapIstanbulReporter: {
-      dir : 'reports/test-results/coverage',
-      reports: {
-        html: 'coverage',
-        lcovonly: 'reports/test-results/coverage/coverage.lcov'
-      }
+    jasmineHtmlReporter: {
+      suppressAll: true // removes the duplicated traces
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, 'reports/coverage/discoteca'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/discoteca'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
     },
-    reporters: ['progress', 'kjhtml','junit'],
-    htmlReporter: {
-      outputFile: 'reports/unit/units.html',
-
-      // Optional
-      pageTitle: 'Vida - Tests Unitarios',
-      subPageTitle: 'Presentacion HTML de los test unitarios',
-      dir: 'reports/html/'
-    },
-    junitReporter: {
-      outputDir: 'reports/unit',
-      outputFile: 'test-results.xml',
-      suite: '',
-      useBrowserName: false
-    },
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['ChromeHeadless'],
-    singleRun: true,
+    singleRun: false,
     restartOnFileChange: true
   });
 };
