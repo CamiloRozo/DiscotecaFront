91d9af2987db177e4f092fdaae4f8b49b15c9006
Camilo Rozo <camilo.rozo@ceiba.com.co>
2022-03-24 16:04:05 -0500

    menu coverage

--- src/app/shared/components/menu/menu.component.spec.ts
+++ src/app/shared/components/menu/menu.component.spec.ts
@@ -1,6 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { MenuComponent } from './menu.component';
+import {MenuComponent} from './menu.component';
 
 describe('MenuComponent', () => {
   let component: MenuComponent;
@@ -8,9 +8,9 @@ describe('MenuComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MenuComponent ]
+      declarations: [MenuComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -18,8 +18,43 @@ describe('MenuComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
+  beforeEach(() => {
+    let store = {};
+    const mockLocalStorage = {
+      getItem: (key: string): string => {
+        // @ts-ignore
+        return key in store ? store[key] : null;
+      },
+      setItem: (key: string, value: string) => {
+        // @ts-ignore
+        store[key] = `${value}`;
+      },
+      removeItem: (key: string) => {
+        // @ts-ignore
+        delete store[key];
+      },
+      clear: () => {
+        store = {};
+      }
+    };
+    spyOn(sessionStorage, 'getItem')
+      .and.callFake(mockLocalStorage.getItem);
+    spyOn(sessionStorage, 'setItem')
+      .and.callFake(mockLocalStorage.setItem);
+    spyOn(sessionStorage, 'removeItem')
+      .and.callFake(mockLocalStorage.removeItem);
+    spyOn(sessionStorage, 'clear')
+      .and.callFake(mockLocalStorage.clear);
+  });
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('recovery session storage', () => {
+    sessionStorage.setItem('id', '5');
+    sessionStorage.setItem('nombre', 'camilo');
+    component.ngOnInit();
+    expect(component.id).toEqual('5');
+    expect(component.nombre).toEqual('camilo');
   });
 });
