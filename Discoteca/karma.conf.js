2d5a01ab745ac14f2f94b1c36f3858c1e9b0b871
Camilo Rozo <camilo.rozo@ceiba.com.co>
2022-03-24 11:02:25 -0500

    test 1

--- Discoteca/karma.conf.js
+++ Discoteca/karma.conf.js
@@ -8,37 +8,47 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-junit-reporter'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
+      require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
-      },
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
+    remapIstanbulReporter: {
+      dir : 'reports/test-results/coverage',
+      reports: {
+        html: 'coverage',
+        lcovonly: 'reports/test-results/coverage/coverage.lcov'
+      }
     },
-    coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/discoteca'),
-      subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
-      ]
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, 'reports/coverage/discoteca'),
+      reports: ['html', 'lcovonly', 'text-summary'],
+      fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml','junit'],
+    htmlReporter: {
+      outputFile: 'reports/unit/units.html',
+
+      // Optional
+      pageTitle: 'Vida - Tests Unitarios',
+      subPageTitle: 'Presentacion HTML de los test unitarios',
+      dir: 'reports/html/'
+    },
+    junitReporter: {
+      outputDir: 'reports/unit',
+      outputFile: 'test-results.xml',
+      suite: '',
+      useBrowserName: false
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['ChromeHeadless'],
-    singleRun: false,
+    singleRun: true,
     restartOnFileChange: true
   });
 };
